97f0746067a8a851295488437022a29c55ded3eb
Thais Freitas <thaisfreitas31@gmail.com>
2025-04-21 16:28:23 -0300

    Add common steps for browser windows feature

---
+++ cypress/e2e/step_definitions/common_steps.js
@@ -0,0 +1,14 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+
+Given('I navigate to the DemoQA website', () => {
+  cy.visit('https://demoqa.com/');
+});
+
+
+Given('I choose the {string} option', (option) => {
+    cy.contains(option).click();
+  });
+
+Given('I click on the {string} submenu', (submenu) => {
+cy.contains(submenu).click();
+});
